ce33e2f617fa686ea3938d894b774141e8315631
Habib Alamin <ha.alamin@gmail.com>
2016-04-15 21:55:39 +0100

    Add mkMove function.
    This function takes a string (representing a keypress by the user), and
    converts it into a Move (representing a game move).

--- Movement.hs
+++ Movement.hs
@@ -1,3 +1,10 @@
 module Movement where
 
 data Move = Up | Down | Left | Right | Noop deriving Show
+
+mkMove :: String -> Move
+mkMove "\ESC[A" = Up
+mkMove "\ESC[B" = Down
+mkMove "\ESC[C" = Movement.Right
+mkMove "\ESC[D" = Movement.Left
+mkMove _        = Noop
