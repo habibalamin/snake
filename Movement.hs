0935aedf8c2d9e48c18bac79bfecc138bfdf7709
Habib Alamin <ha.alamin@gmail.com>
2016-04-15 22:42:45 +0100

    Add getNextMove IO action.
    This computation waits 100k microseconds (which is a magic number that
    just felt right), then returns the user's next move. It is intended to
    be used through rapidly repeated calls to keep getting the user's next
    move.

--- Movement.hs
+++ Movement.hs
@@ -1,5 +1,9 @@
 module Movement where
 
+import System.IO (hReady, stdin)
+import Control.Concurrent (threadDelay)
+import Control.Monad (replicateM)
+
 data Move = Up | Down | Left | Right | Noop deriving Show
 
 mkMove :: String -> Move
@@ -8,3 +12,17 @@ mkMove "\ESC[B" = Down
 mkMove "\ESC[C" = Movement.Right
 mkMove "\ESC[D" = Movement.Left
 mkMove _        = Noop
+
+getNextMove :: IO Move
+getNextMove = mkMove <$> keys where
+    -- MAGICNUMBER: this feels like a good pace when repeated, but it
+    -- should probably be customisable & labelled for why I chose it?
+    -- SRP: I think this function does too much, and assumes too much
+    -- about its callers.
+    keys = threadDelay 100000 >> hReady stdin >>= getFirstChar >>= getRestChars >>= flushInput where
+        getFirstChar ready = if ready then sequence [getChar] else return "\NUL"
+        getRestChars firstChar = if firstChar == "\ESC" then
+            (firstChar ++) <$> replicateM 2 getChar else
+            return firstChar
+        flushInput caughtChars = hReady stdin >>=
+            \ready -> if ready then getChar >> flushInput caughtChars else return caughtChars
