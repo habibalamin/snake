0825952cfa185ac14a21b491b4003dcae9679a53
Habib Alamin <ha.alamin@gmail.com>
2016-04-15 23:26:48 +0100

    Add getTerminalWidthHeight IO action.
    The getTerminalWidthHeight computation returns the terminal width and
    height in a tuple.

--- Terminal.hs
+++ Terminal.hs
@@ -6,8 +6,16 @@ import System.IO (stdin
                 , hSetEcho
                 , hSetBuffering
                 , BufferMode(..))
+import Data.Maybe (fromJust)
+
+import qualified System.Console.Terminal.Size as T
 
 catchInput :: IO a -> IO a
 catchInput action = (,) <$> hGetEcho stdin <*> hGetBuffering stdin >>=
     \(echo, buffering) -> hSetEcho stdin False >> hSetBuffering stdin NoBuffering >> action >>=
     \result -> hSetEcho stdin echo >> hSetBuffering stdin buffering >> return result
+
+getTerminalWidthHeight :: Integral a => IO (a, a)
+getTerminalWidthHeight = maybeWindowToWidthHeight <$> T.size where
+    maybeWindowToWidthHeight maybeWindow = (T.width window, T.height window) where
+        window = fromJust maybeWindow
