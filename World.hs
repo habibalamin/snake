1374153eeaf0272fc7030c897d13a98cad436d28
Habib Alamin <ha.alamin@gmail.com>
2016-04-16 23:43:50 +0100

    Manually write Show instance for World, don't derive.
    Update the randomWorld function's width & height so that it has enough
    space to draw the edges of the world in the terminal without wrapping.

--- World.hs
+++ World.hs
@@ -2,16 +2,36 @@ module World where
 
 import System.Random (randomRIO)
 
+import Data.List.Split (chunksOf)
+
 import Terminal
 import Movement
 import Snake
 
 data World = World { worldWidth :: Integer
                    , worldHeight :: Integer
-                   , worldSnake :: Snake } deriving Show
+                   , worldSnake :: Snake }
+
+instance Show World where
+    -- OPTIMIZE: this is a hella long function.
+    show World { worldWidth = width 
+               , worldHeight = height
+               , worldSnake = snake } = showChunked $ map drawPoint rowReverseList where
+        rowReverseList = concat $ reverse $ chunksOf (fromIntegral width) [0..(size - 1)]
+        size = width * height
+
+        drawPoint index = if snakeBodyIsAt index snake then 'o' else ' '
+        snakeBodyIsAt index snake = indexToCoords width index `elem` getCoords snake
+
+        showChunked list = boxWorld $ chunksOf (fromIntegral width) list
+        boxWorld lineList = "/" ++ horizontalEdges ++ "\\\n" ++
+            concatMap (\line -> "|" ++ line ++ "|\n") lineList ++
+            "\\" ++ horizontalEdges ++ "/"
+        horizontalEdges = replicate (fromIntegral width) '-'
 
 randomWorld :: IO World
-randomWorld = getTerminalWidthHeight >>= return . newWorld >>= seedWorld
+randomWorld = getTerminalWidthHeight >>= (\(x,y) -> return (x - 2, y - 2)) >>=
+    return . newWorld >>= seedWorld
 
 newWorld :: Integral a => (a, a) -> World
 newWorld (width, height) = World { worldWidth = fromIntegral width
