7dcb99a4edd68c524de76a8a40bd6477b1234163
Habib Alamin <ha.alamin@gmail.com>
2016-04-17 19:47:57 +0100

    Amend snake function to clear world after each print.
    Also, don't print a newline after each print of the world. This avoids
    the blank line after the bottom border of the world.

--- Main.hs
+++ Main.hs
@@ -8,4 +8,14 @@ main :: IO ()
 main = catchInput $ randomWorld >>= snake
 
 snake :: World -> IO ()
-snake world = print world >> getNextMove >>= return . flip updateWorld world >>= snake
+snake world = printWorld world >>
+    getNextMove >>= updateWorldIO world >>=
+    clearScreenWorldPassthru >>= snake where
+        printWorld = putStr . show
+        updateWorldIO world = return . flip updateWorld world
+        clearScreenWorldPassthru world = clearScreen world >> return world where
+            -- We add 1 to the height to account for the top border. We
+            -- are already on the same line as the bottom border (as we
+            -- use putStr to avoid printing a newline after the world),
+            -- so no need to need to account for that.
+            clearScreen world = putStr $ "\r\ESC[" ++ show (worldHeight world + 1) ++ "A"
