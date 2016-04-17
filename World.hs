858a845f5783f752da176b417160d12d8498f065
Habib Alamin <ha.alamin@gmail.com>
2016-04-17 16:57:59 +0100

    Add updateWorld function to World module.
    This function takes a Move and a World and returns a new World with a
    moved snake (it requires the whole world, not just the snake, so that
    it can check if the snake is going out of the bounds of the world).

--- World.hs
+++ World.hs
@@ -43,3 +43,34 @@ newWorld (width, height) = World { worldWidth = fromIntegral width
 seedWorld :: World -> IO World
 seedWorld world = (,) <$> randomRIO (0, worldWidth world) <*> randomRIO (0, worldHeight world) >>=
     \(x, y) -> return world { worldSnake = Snake North [Coords x y] }
+
+-- OPTIMIE: this is a very large function (albeit mainly pattern
+-- matching and other simple constructs). It seems to be working
+-- well, however, it is not unlikely that I've missed something.
+updateWorld :: Move -> World -> World
+updateWorld move world@World { worldWidth = width
+                             , worldHeight = height
+                             , worldSnake = snake } = world { worldSnake = updateSnake
+    move width height snake } where
+        updateSnake m w h Snake { getDirection = d
+                                , getCoords = c } = Snake { getDirection = newDirection m d
+                                                          , getCoords = newCoords m w h d c } where
+            newDirection Noop           d = d
+            newDirection Up             d = if d == South then d else North
+            newDirection Down           d = if d == North then d else South
+            newDirection Movement.Left  d = if d == East then d else West
+            newDirection Movement.Right d = if d == West then d else East
+            newCoords m w h d coords = let newHeadCoords = updateHead m w h d (head coords) in
+                newHeadCoords : updateTail newHeadCoords coords where
+                    updateTail nhc cs = if length cs == 1 then []
+                        else if nhc == head cs then tail cs else init cs
+                    updateHead Noop w h d head = progressHead w h d head where
+                        progressHead w h d head = if outOfBounds w h $ newHead d head
+                            then head
+                            else newHead d head where
+                            outOfBounds w h head = head > (indexToCoords w $ (w * h) - 1)
+                            newHead North (Coords x y) = Coords x (y + 1)
+                            newHead East  (Coords x y) = Coords (x + 1) y
+                            newHead South (Coords x y) = Coords x (y - 1)
+                            newHead West  (Coords x y) = Coords (x - 1) y
+                    updateHead _    w h d head = head
